b8da231f91fd12b80cc29a1dd70fc18ede0e9653
PowersRanger <john.89.powers@gmail.com>
2023-02-12 12:59:48 -0700

    updated project Readme file

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+console.log("Here are the topics we learned through Prework");
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
+
 function listTopics() {
  for (var x = 0; x < topics.length; x++) {
    console.log(topics[x]);
